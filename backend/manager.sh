5d1ac59d3c25b2e5bf11a7e8e8f739bfb66ad48b
Enzo José Rodrigues Jiotoli Tasso de Oliveira <149714801+TassoEnzo@users.noreply.github.com>
2026-06-16 02:17:03 -0300

    Update manager.sh

--- backend/manager.sh
+++ backend/manager.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
+export DOCKER_HOST=unix:///var/run/docker.sock
 dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &
 
 echo '[1/6] Aguardando Docker Engine...'
-until docker info > /dev/null 2>&1; do sleep 1; done
+sleep 10
+until docker -H unix:///var/run/docker.sock info > /dev/null 2>&1; do
+  echo '[1/6] Ainda aguardando...'
+  sleep 2
+done
 
 echo '[2/6] Buildando backend...'
 docker build -t epinfo_backend:latest /epinfo/backend
@@ -11,12 +16,17 @@ docker build -t epinfo_backend:latest /epinfo/backend
 echo '[3/6] Buildando frontend...'
 docker build -t epinfo_frontend:latest /epinfo/frontend
 
+echo '[3.5/6] Buildando banco...'
+docker build -t epinfo_db:latest /epinfo/banco_de_dados
+
 echo '[4/6] Exportando imagens para workers...'
-docker save epinfo_backend:latest epinfo_frontend:latest -o /epinfo/images/epinfo.tar
+docker save epinfo_backend:latest epinfo_frontend:latest epinfo_db:latest -o /epinfo/images/epinfo.tar
 touch /epinfo/images/ready
 
 echo '[5/6] Inicializando Swarm...'
-docker swarm init --advertise-addr eth0
+MANAGER_IP=$(getent hosts manager | awk '{ print $1 }')
+echo "Manager IP: $MANAGER_IP"
+docker swarm init --advertise-addr $MANAGER_IP --listen-addr 0.0.0.0:2377
 
 docker swarm join-token worker -q > /epinfo/token/join.token.tmp
 cat /epinfo/token/join.token.tmp | sed 's/[^A-Za-z0-9:_-]//g' > /epinfo/token/join.token
@@ -28,5 +38,6 @@ until [ "$(docker node ls --filter role=worker -q 2>/dev/null | wc -l)" -ge 2 ];
 done
 echo 'Workers prontos!'
 docker stack deploy --resolve-image never -c /epinfo/docker-stack.yml epinfo
+echo 'Cluster pronto!'
 
 tail -f /dev/null
